5ea9935cc4e7f90a2ad557bff5fbbdbc2871481d
Balaji Sivaraman <balaji@balajisivaraman.com>
2024-05-07 10:55:26 +0530

    testing

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ variable "k8s_namespace" {
   type = string
 }
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
-}
-
 resource "kubernetes_namespace" "example" {
   metadata {
     name = var.k8s_namespace
